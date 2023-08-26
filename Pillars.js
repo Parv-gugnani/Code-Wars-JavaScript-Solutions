bc44d024c6f8cd1571dea7acb232656300bed1be
Parv-gugnani <9765.parv@gmail.com>
2023-08-26 17:41:09 +0530

    Pillars

--- Pillars.js
+++ Pillars.js
@@ -2,6 +2,6 @@ function pillars(numPill, dist, width) {
   if (numPill <= 1 || dist < 10 || dist > 30 || width < 10 || width > 50) {
     return 0;
   }
-  const distance = (numPill - 1) * dist + (numPill - 2) * width;
+  const distance = (numPill - 1) * dist * 100 + (numPill - 2) * width;
   return distance;
 }
